4af71d9e00d9798c1c2aec9dce86a579a48c89aa
pozytron <michal.tymula@coderslab.pl>
2020-08-04 12:14:29 +0200

    config created

---
+++ src/config.sh
@@ -0,0 +1,11 @@
+# Space separated Array with github usernames, ex. user1 user2 user3
+users=(AnnaWolska SlawomirJurak MaciejMajchrzak cerassus garryCold kamilkrysiak MiloszDziadosz mkazimie jarmatys piotrgra RafalDoranczyk KatBotkowska tomzdu Vladek23 ZWasilonek pielarz )
+
+repos=("REPO_1" "REPO_1" "FSB_O_Exam" "QUIT")
+
+#currentGroup=${PWD##*/}
+currentGroup="WAR_FSB_O_05"
+# bg_default=${On_Blue}
+# bg_selected=${BYellow}
+# bg_error=${BRed}
+# bg_success=${BGreen}
